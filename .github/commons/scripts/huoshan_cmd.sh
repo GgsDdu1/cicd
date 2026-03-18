190f46c38f4ffa149557f7585b4286aaeda03d7e
zhangyingyi <zhangyingyi@senseauto.com>
2026-03-18 09:23:04 +0800

    fix ci

--- .github/commons/scripts/huoshan_cmd.sh
+++ .github/commons/scripts/huoshan_cmd.sh
@@ -4,11 +4,11 @@ export http_proxy
 export https_proxy
 
 echo "commit_id: $commit_id"
-git clone --depth 1 https://github.com/GgsDdu1/cicd.git /root/code/kairos-sensenova
-# git clone --depth 1 https://github.com/kairos-agi/kairos-sensenova.git /root/code/kairos-sensenova
+git clone --depth 1 https://github.com/kairos-agi/kairos-sensenova.git /root/code/kairos-sensenova
 cd /root/code/kairos-sensenova
-git fetch --depth 1 origin $commit_id  # 获取指定 commit（及其必要历史）
-git checkout $commit_id
+git fetch --depth 1 origin "$commit_id"
+git checkout "$commit_id"
+git lfs pull
 ls -alh
 # 测试运行的命令
 echo "Run cmd: {$case_cmd}"
--- .github/commons/scripts/sensecore_cmd.sh
+++ .github/commons/scripts/sensecore_cmd.sh
@@ -44,11 +44,12 @@ touch "$log_path"
 {
     source /data/swj/myenv/bin/activate
     echo "commit_id: $commit_id"
-    git clone --depth 1 https://github.com/GgsDdu1/cicd.git /root/code/kairos-sensenova
-    # git clone --depth 1 https://github.com/kairos-agi/kairos-sensenova.git /root/code/kairos-sensenova
+    apt install git-lfs
+    git clone --depth 1 https://github.com/kairos-agi/kairos-sensenova.git /root/code/kairos-sensenova
     cd /root/code/kairos-sensenova
     git fetch --depth 1 origin "$commit_id"
     git checkout "$commit_id"
+    git lfs pull
     ls -alh
     echo "Run cmd: $case_cmd"
     $case_cmd
